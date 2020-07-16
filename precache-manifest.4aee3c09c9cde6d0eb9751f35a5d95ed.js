b97dde6314b3475befa021437c7f9b31b8938054
Grasscopper <jssstransky@gmail.com>
2020-07-16 12:40:17 -0400

    Updates

---
+++ precache-manifest.4aee3c09c9cde6d0eb9751f35a5d95ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3efa535ab696e92082cff2a4d5c550e5",
+    "url": "/hackathon072020/index.html"
+  },
+  {
+    "revision": "e3352560e35021d1ca21",
+    "url": "/hackathon072020/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "dad281d31e7fdb133106",
+    "url": "/hackathon072020/static/js/2.48f752de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hackathon072020/static/js/2.48f752de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3352560e35021d1ca21",
+    "url": "/hackathon072020/static/js/main.e725361d.chunk.js"
+  },
+  {
+    "revision": "55462581de0711ebfc97",
+    "url": "/hackathon072020/static/js/runtime-main.696cfc24.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hackathon072020/static/media/logo.5d5d9eef.svg"
+  }
+]);
