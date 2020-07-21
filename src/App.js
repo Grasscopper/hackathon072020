51f354ad6f62918f394cea2a88ae45321c8b01d9
Grasscopper <jssstransky@gmail.com>
2020-07-21 12:42:02 -0400

    background is now fixed in place and green edges are removed

--- src/App.js
+++ src/App.js
@@ -7,10 +7,7 @@ const App = (props) => {
   parallaxInstance.friction(0.4, 0.4)
   parallaxInstance.limit(35.0, 35.0)
   return (
-    <>
-    <p>All credit for characters goes to Nintendo</p>
-    <p>And a big thank you to freepngimg.com</p>
-    </>
+    <></>
   )
 }
 
